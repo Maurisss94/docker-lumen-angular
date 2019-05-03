97440c62d81e1f1d422f2d5fbdbc648a2970fd8d
Mauro Balestra <mbalestra94@gmail.com>
2019-05-03 15:56:38 +0200

    Adding images, and refactoring controllerAs.

--- frontend/app/view1/view1.js
+++ frontend/app/view1/view1.js
@@ -5,29 +5,21 @@ angular.module('myApp.view1', ['ngRoute'])
     .config(['$routeProvider', function ($routeProvider) {
         $routeProvider.when('/view1', {
             templateUrl: 'view1/view1.html',
-            controller: 'View1Ctrl'
+            controller: 'View1Ctrl',
+            controllerAs: 'view'
         });
     }])
 
-    .controller('View1Ctrl', ['$scope', settingsController]);
+    .controller('View1Ctrl', ['$http', settingsController]);
 
-function settingsController($scope) {
+function settingsController($http) {
 
-    $scope.name = "Mauro Balestra";
-    $scope.phones = [
-        {
-            name: 'Nexus S',
-            snippet: 'Fast just got faster with Nexus S.',
-            age: 1
-        }, {
-            name: 'Motorola XOOM™ with Wi-Fi',
-            snippet: 'The Next, Next Generation tablet.',
-            age: 2
-        }, {
-            name: 'MOTOROLA XOOM™',
-            snippet: 'The Next, Next Generation tablet.',
-            age: 3
-        }
-    ];
-    this.orderProp = 'age';
+    let self = this;
+    self.name = "Mauro Balestra";
+    console.log($http);
+
+    $http.get('phones/phones.json').then(function (response) {
+        self.phones = response.data;
+    });
+    self.orderProp = 'age';
 }
